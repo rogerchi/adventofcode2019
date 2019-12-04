babd9b879cc5561fa77cda2839df19f116e97005
Roger Chi <roger@rogerchi.com>
2019-12-04 10:59:39 -0500

    add range parameter

--- 04/pt2/index.js
+++ 04/pt2/index.js
@@ -5,9 +5,9 @@ const range = [353096, 843212];
 const validPasswords = [];
 
 for (let i = range[0]; i <= range[1]; i++) {
-  if (isValidPassword(i)) {
+  if (isValidPassword(i, range)) {
     validPasswords.push(i);
   }
 }
 
-console.log(validPasswords.length, validPasswords);
+console.log(validPasswords.length);
